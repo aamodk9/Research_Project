0224854dcb8be683bdbb5297eebb2f1b411f5cc6
Your Name <aamodk9@gmail.com>
2020-11-30 22:57:55 +0530

    first update

--- Research project.py
+++ Research project.py
@@ -6,40 +6,40 @@
 
 import os
 from PIL import Image
-from skimage import io
 import glob
 from matplotlib import pyplot as plt
 import PIL
 import pandas as pd
-import matplotlib
 %matplotlib inline
 
-imgs=glob.glob(r'D:\\Study\\Sem-3\\Research project\\Images\\**\*.tif', recursive=True)
+images=glob.glob(r'D:\\Study\\Sem-3\\Research project\\Images\\**\*.tif', recursive=True)
 
 #Main 3 Csv's 
 csv1= pd.read_csv('D:\Study\Sem-3\Research project\Research project\CSV files\A01Sample-1.csv')
-csv2= pd.read_csv('D:\Study\Sem-3\Research project\Research project\CSV files\A02-tracks.csv')
+csv2= pd.read_csv('D:\Study\Sem-3\Research project\Research project\CSV files\A02-tracks.csv',sep=";")
 csv3= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\A03-tracks.csv', sep=";")
 
-#Extra files
-csv4= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\example_imgs.csv',sep=";")
-csv5= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\example_imgs-1.csv',sep=";")
 
 #Plotting of images
 rows=5
-for i, x in enumerate(imgs):
+for i, x in enumerate(images):
     img = PIL.Image.open(x)
     plt.subplot(rows,3,i+1)
     plt.axis('off')
     plt.imshow(img)
     
 
-
 #Conversion for csv1
 csv1['Slice n°']=csv1['Slice n°'].div(3)
 csv1['Slice n°'] = csv1['Slice n°'].astype(int)
 display(csv1.dtypes) 
 csv1.head()
+
+#Conversion for csv2
+csv2['Slice n°']=csv2['Slice n°'].div(3)
+csv2['Slice n°'] = csv2['Slice n°'].astype(int)
+display(csv2.dtypes) 
+csv2.head()
 
 #conversion for csv3
 csv3['Slice n°']=csv3['Slice n°'].div(3)
