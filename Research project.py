cae91a25ff0b6d144b1401600827d70baa1fe582
Your Name <aamodk9@gmail.com>
2020-11-30 16:13:11 +0530

    first update

---
+++ Research project.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: AAMOD
+"""
+
+import os
+from PIL import Image
+from skimage import io
+import glob
+from matplotlib import pyplot as plt
+import PIL
+import pandas as pd
+import matplotlib
+%matplotlib inline
+
+imgs=glob.glob(r'D:\\Study\\Sem-3\\Research project\\Images\\**\*.tif', recursive=True)
+
+#Main 3 Csv's 
+csv1= pd.read_csv('D:\Study\Sem-3\Research project\Research project\CSV files\A01Sample-1.csv')
+csv2= pd.read_csv('D:\Study\Sem-3\Research project\Research project\CSV files\A02-tracks.csv')
+csv3= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\A03-tracks.csv', sep=";")
+
+#Extra files
+csv4= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\example_imgs.csv',sep=";")
+csv5= pd.read_csv(r'D:\Study\Sem-3\Research project\Research project\CSV files\example_imgs-1.csv',sep=";")
+
+#Plotting of images
+rows=5
+for i, x in enumerate(imgs):
+    img = PIL.Image.open(x)
+    plt.subplot(rows,3,i+1)
+    plt.axis('off')
+    plt.imshow(img)
+    
+
+
+#Conversion for csv1
+csv1['Slice n°']=csv1['Slice n°'].div(3)
+csv1['Slice n°'] = csv1['Slice n°'].astype(int)
+display(csv1.dtypes) 
+csv1.head()
+
+#conversion for csv3
+csv3['Slice n°']=csv3['Slice n°'].div(3)
+csv3['Slice n°'] = csv3['Slice n°'].astype(int)
+display(csv3.dtypes) 
+csv3.head()
